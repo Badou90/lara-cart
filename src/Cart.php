947923c4a338398f6f716f0e2e80793736e12e0b
badou <badou@yandex.ru>
2016-03-06 12:54:54 +0900

    common add, remove, clear, update methods of cart

--- src/Cart.php
+++ src/Cart.php
@@ -4,14 +4,72 @@ namespace Badou\Cart;
 
 class Cart {
     protected $session;
+    protected $instance;
 
     public function __construct($session)
     {
         $this->session = $session;
+        $this->instance = 'cart';
+    }
+
+    public function instance($instance = null)
+    {
+        $this->instance = $instance;
+        return $this;
     }
 
     public function content()
     {
-        return 'content';
+        return $this->session->has($this->getInstance()) ? $this->session->get($this->getInstance()) : new \Illuminate\Support\Collection;
+    }
+
+    public function clear()
+    {
+        $cart = $this->content();
+        $cart->forget($this->getInstance());
+    }
+
+    public function add($item, $qty)
+    {
+        $cart = $this->content();
+
+        if($cart->has($item->id))
+            $cart = $this->addItem($item, $qty + $this->getItem($item->id)->qty);
+        else
+            $cart = $this->addItem($item, $qty);
+
+        $this->updateCart($cart);
+    }
+
+    public function addItem($item, $qty)
+    {
+        $cart = $this->content();
+
+        $item->qty = $qty;
+        $cart->put($item->id, $item);
+
+        return $cart;
+    }
+
+    public function remove($item)
+    {
+        $cart = $this->content();
+        $cart->forget("$this->getInstance().$item->id");
+    }
+
+    public function getInstance()
+    {
+        return 'laracart'.$this->instance;
+    }
+
+    public function getItem($id)
+    {
+        $cart = $this->content();
+        return $cart->get($id);
+    }
+
+    public function updateCart($cart)
+    {
+        return $this->session->set($this->getInstance(), $cart);
     }
 }
